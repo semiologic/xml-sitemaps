cda68c95134454017b9d9bf1110f06834c10ce31
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-09-06 11:53:54 +0000

    bump xml-sitemaps
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/xml-sitemaps/trunk@2540 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- xml-sitemaps.php
+++ xml-sitemaps.php
@@ -3,7 +3,7 @@
 Plugin Name: XML Sitemaps
 Plugin URI: http://www.semiologic.com/software/xml-sitemaps/
 Description: Automatically generates XML Sitemaps for your site and notifies search engines when they're updated.
-Version: 1.3 RC2
+Version: 1.3
 Author: Denis de Bernardy
 Author URI: http://www.getsemiologic.com
 Text Domain: xml-sitemaps
